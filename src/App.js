4cb0f464e9f0145656593e0d80e210ad5bf56c74
jose <jlguambo@gmail.com>
2022-05-06 14:02:31 -0500

    fix/remove-unused-variables

--- src/App.js
+++ src/App.js
@@ -20,8 +20,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+initializeApp(firebaseConfig);
+//const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
 
 function App() {
   return (
